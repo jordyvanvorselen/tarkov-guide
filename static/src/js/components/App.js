f4437d0ffb3147408cf9cd95e7b00cc2248aa866
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-10-02 20:33:11 +0200

    Add Cypress

---
+++ static/src/js/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "./Map";
+
+const App = () => {
+  return (
+    <div className="bg-slate-900 h-full w-full">
+      <Map />
+    </div>
+  );
+};
+
+export default App;
