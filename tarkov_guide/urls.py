5d345eeff5c9dde772b5d405d6f9236122f3a0e7
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-10-09 20:32:22 +0200

    Add mypy as static type checker

--- tarkov_guide/urls.py
+++ tarkov_guide/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from typing import List, Union
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
@@ -21,4 +23,4 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("questmap.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # type: ignore
