0838a3a3483cefb2534e84176cf9481c1f832188
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-10-09 17:44:39 +0200

    Create feature tests for quest selection

--- cypress/features/user_can_select_quests.cy.js
+++ cypress/features/user_can_select_quests.cy.js
@@ -4,4 +4,27 @@ describe("when the user arrives on the home page", () => {
 
     cy.get(".quest-selector");
   });
+
+  it("he can filter the quests based on name", () => {
+    cy.visit("/");
+
+    const questSelector = cy.get(".quest-selector");
+
+    questSelector.get("input[placeholder*='Filter quests']").type("Operati");
+
+    questSelector.contains("Operation Aquarius - Part 1").should("be.visible");
+    questSelector.contains("Shortage").should("not.exist");
+  });
+
+  it("he can select multiple quests", () => {
+    cy.visit("/");
+
+    const questSelector = cy.get(".quest-selector");
+
+    const firstQuest = questSelector.contains("Painkiller").click();
+    const secondQuest = questSelector.contains("Shortage").click();
+
+    firstQuest.should("have.class", "selected");
+    secondQuest.should("have.class", "selected");
+  });
 });
